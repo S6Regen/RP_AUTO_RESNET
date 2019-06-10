e978b9f06117ae4a9a9a8b815689d9cc49cb5809
S6Regen <S6Regen@users.noreply.github.com>
2019-06-10 12:06:50 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.event.*;
 import java.awt.image.BufferedImage;
 
 public class Main {
@@ -9,6 +10,7 @@ public class Main {
   static JLabel costLb;
   
   public static void main(String[] args){    
+    
      JFrame f=new JFrame("RP Neural");
      f.setLayout(null);
      f.setBounds(100,100,273,355);    
@@ -21,7 +23,7 @@ public class Main {
      JLabel costTxt=new JLabel("Cost:");
      costTxt.setBounds(5,303,50,30);
      f.add(costTxt);
-     costLb=new JLabel("Hi");
+     costLb=new JLabel("___");
      costLb.setBounds(55,303,200,30);
      f.add(costLb);   
      buffer=new BufferedImage(256,256,BufferedImage.TYPE_INT_RGB);
@@ -29,6 +31,64 @@ public class Main {
      imgLb.setBounds(5,5,256,256);
      f.add(imgLb);
      f.setVisible(true);
+     ActionListener aL=new ActionListener() {
+       public void actionPerformed(ActionEvent e) {
+        System.out.println(e);
+       }
+     };
+     trainBtn.addActionListener(aL);
+     recallBtn.addActionListener(aL);
+  }  
+  
+  class RPN {
+    
+    RPN(int vecLen,int density,int depth){
+    
+    }
+    
+    recall(float[] result,float[] input){
+     
+    }
+    
   }  
  
+  static void wht(float[] vec) {
+    int i, j, hs = 1, n = vec.length;
+    while (hs < n) {
+      i = 0;
+      while (i < n) {
+        j = i + hs;
+        while (i < j) {
+          float a = vec[i];
+          float b = vec[i + hs];
+          vec[i] = a + b;
+          vec[i + hs] = a - b;
+          i += 1;
+        }
+        i += hs;
+      }
+      hs += hs;
+    }
+    float sc = 1f / (float) Math.sqrt(n);
+    for (i = 0; i < n; i++) {
+      vec[i] *= sc;
+    }
+  }
+
+  static void signFlip(float[] vec, long h) {
+    h = h * 2862933555777941757L + 3037000493L;
+    for (int i = 0; i < vec.length; i++) {
+      h = h * 2862933555777941757L + 3037000493L;
+      int x = (int) (h >>> 32) & 0x80000000;  // select sign flag bit
+      vec[i] = Float.intBitsToFloat(x ^ Float.floatToRawIntBits(vec[i]));  // xor top bit
+    }
+  }
+
+  //  Fast random projection.  
+  static void fastRP(float[] vec, long h) {
+    signFlip(vec, h);
+    wht(vec);
+  }
+  
+  
 }  
